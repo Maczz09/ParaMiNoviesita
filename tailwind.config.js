da361f627b555f0e9b566d91c2b152d10c170319
Maczz09 <u22201741@utp.edu.pe>
2024-10-30 22:59:16 -0500

    actualizando

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // Incluye el archivo HTML en la raíz
+    './src/**/*.{html,js}', // Incluye archivos en src con extensiones html y js
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
